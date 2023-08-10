b17b25d5d5882bbed6ee4682444ff5bcd19741c9
brave-12 <1310565983@qq.com>
2023-08-10 16:10:41 +0800

    1

--- webpack_code/src/main.js
+++ webpack_code/src/main.js
@@ -3,6 +3,9 @@ import sum from './js/sum'
 // 想要webpack打包资源。必须引入该资源   webpack不能识别css文件所以 index.css 写全称
 import './css/index.css'
 import './less/index.less'
+import './sass/index.sass'
+import './sass/index.scss'
+import './stylus/index.styl'
 
 console.log(count(1,2));
 console.log(sum(1,2,3,4));
